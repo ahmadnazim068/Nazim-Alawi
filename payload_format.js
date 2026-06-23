24b9c7592892795c1c2af64100acabcb90b481b1
ahmadnazim068 <ahmadnazim068@gmail.com>
2026-06-23 10:09:11 +0800

    Create payload_format.js

---
+++ payload_format.js
@@ -0,0 +1,27 @@
+{
+  "project": "Smart Fun Run",
+  "instruction": "All teams must send data to the Dashboard via POST request to /api/update using the following JSON structures depending on your team ID.",
+  
+  "Team_1_Registration": {
+    "team_id": 1,
+    "checked_in": 35,
+    "total_runners": 50
+  },
+  
+  "Team_2_Tracking": {
+    "team_id": 2,
+    "leader_time": "00:15:32",
+    "checkpoint_id": "CP_3"
+  },
+  
+  "Team_3_Environmental": {
+    "team_id": 3,
+    "sos_triggered": false,
+    "temperature_celsius": 32.5
+  },
+  
+  "Team_4_Hydration": {
+    "team_id": 4,
+    "water_level_percent": 85
+  }
+}
